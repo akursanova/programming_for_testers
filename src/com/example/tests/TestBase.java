f3724539805aa3755bd75d46d802c2fd67f4ab5c
akursanova <kursandra@gmail.com>
2014-03-12 01:32:33 +0400

    homework1

--- src/com/example/tests/TestBase.java
+++ src/com/example/tests/TestBase.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import static org.testng.Assert.*;
 
 public class TestBase {
 
